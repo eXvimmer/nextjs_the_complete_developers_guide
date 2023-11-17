34360a5ac06f0d1eac252058c4b5febb0a508d7d
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-17 20:28:02 +0330

    01: add images.

--- 01_corp/src/app/page.tsx
+++ 01_corp/src/app/page.tsx
@@ -1,7 +1,20 @@
+import Image from "next/image";
+import HomeImage from "public/home.jpg";
+
 export default function Home() {
   return (
     <div>
-      <h1>Home Page</h1>
+      Home Page
+      <div className="absolute inset-0 -z-10">
+        <Image
+          src={HomeImage}
+          alt="car factory"
+          fill
+          style={{
+            objectFit: "cover",
+          }}
+        />
+      </div>
     </div>
   );
 }
