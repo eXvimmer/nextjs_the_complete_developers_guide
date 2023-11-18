8ea09d55c07a71355b3710642e15c2a5c32d2504
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2023-11-18 10:06:11 +0330

    02: add a creation form.

--- 02_snippets/src/app/snippets/new/page.tsx
+++ 02_snippets/src/app/snippets/new/page.tsx
@@ -1,5 +1,35 @@
 function SnippetCreatePage() {
-  return <div>Create a snippet</div>;
+  return (
+    <form>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">
+            Title
+          </label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+        <div className="flex gap-4">
+          <label htmlFor="code" className="w-12">
+            Code
+          </label>
+          <textarea
+            id="code"
+            name="code"
+            className="border rounded p-2 w-full"
+          />
+        </div>
+        <button className="rounded p-2 bg-blue-200" type="submit">
+          Create
+        </button>
+      </div>
+    </form>
+  );
 }
 
 export default SnippetCreatePage;
